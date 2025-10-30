59795f416bc220d6c30355ede8d84520057978e7
Ido Katzenellenbogen <idokatze@gmail.com>
2025-10-30 17:08:17 +0200

    Commit 1 - initialize files and connect them.

---
+++ js/book.service.js
@@ -0,0 +1,14 @@
+'use script'
+
+const gBooks = [
+    { title: 'Alice in Wonderland', price: 120 },
+    { title: '1984', price: 100 },
+    { title: 'The Lord of the Rings', price: 180 },
+    { title: 'To Kill a Mockingbird', price: 90 },
+    { title: 'The Great Gatsby', price: 110 },
+]
+
+
+function getBooks() {
+    return (gBooks)
+}
